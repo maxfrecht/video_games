6fea16378a6ccd5bbb3b20065ab72b93c036832d
Maxime Freychet <max.frecht@gmail.com>
2021-07-28 17:44:56 +0200

    start creating front for forums

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ForumRepository;
+use App\Repository\TopicRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,17 +13,21 @@ class ForumController extends AbstractController
 {
     private ForumRepository $forumRepository;
     private EntityManagerInterface $em;
+    private TopicRepository $topicRepository;
 
     /**
      * ForumController constructor.
      * @param ForumRepository $forumRepository
      * @param EntityManagerInterface $em
+     * @param TopicRepository $topicRepository
      */
-    public function __construct(ForumRepository $forumRepository, EntityManagerInterface $em)
+    public function __construct(ForumRepository $forumRepository, EntityManagerInterface $em, TopicRepository $topicRepository)
     {
         $this->forumRepository = $forumRepository;
         $this->em = $em;
+        $this->topicRepository = $topicRepository;
     }
+
 
     #[Route('/forum', name: 'forum')]
     public function index(): Response
@@ -32,5 +37,17 @@ class ForumController extends AbstractController
         return $this->render('forum/index.html.twig', [
             'forums' => $forums,
         ]);
+    }
+
+    #[Route('/forum/topics/{id}', name: 'forum_topics')]
+    public function topicByForum(int $id): Response
+    {
+        $forum = $this->forumRepository->find($id);
+        $topics = $this->topicRepository->findBy(['forum' => $forum]);
+        return $this->render('forum/forum.topics.html.twig', [
+            'forum' => $forum,
+            'topics' => $topics
+        ]);
+
     }
 }
